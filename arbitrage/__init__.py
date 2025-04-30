e4a4e66d931531dbc14efff76528e4f974635d70
zarukonu <dmitry.ahapov@gmail.com>
2025-04-30 21:50:15 +0300

    Update __init__.py

---
+++ arbitrage/__init__.py
@@ -0,0 +1,5 @@
+# arbitrage/__init__.py
+from arbitrage.finder import ArbitrageFinder
+from arbitrage.opportunity import ArbitrageOpportunity
+
+__all__ = ['ArbitrageFinder', 'ArbitrageOpportunity']
